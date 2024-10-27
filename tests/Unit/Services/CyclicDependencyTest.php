3037234102c3aa79e5a4275a59d58215bfd56ecf
DeGraciaMathieu <degracia.mathieu@gmail.com>
2024-10-27 09:42:22 +0100

    improving the completeness of Cyclic Dependency tests

--- tests/Unit/Services/CyclicDependencyTest.php
+++ tests/Unit/Services/CyclicDependencyTest.php
@@ -22,3 +22,25 @@ test('it detects cyclic dependencies', function () {
         ['A', 'B', 'C'],
     ]);
 });
+
+test('it detects classes with multiple cycles', function () {
+
+    $dependencyAggregator = $this->oneDependencyAggregator()
+        ->addClassDependencies(
+            $this->oneClassDependencies()->withFqcn('A')->withDependencies(['B', 'C'])->build()
+        )
+        ->addClassDependencies(
+            $this->oneClassDependencies()->withFqcn('B')->withDependencies(['A'])->build()
+        )
+        ->addClassDependencies(
+            $this->oneClassDependencies()->withFqcn('C')->withDependencies(['A'])->build()
+        )
+        ->build();
+
+    $cycles = app(CyclicDependency::class)->detect($dependencyAggregator->classes());
+
+    expect($cycles)->toBe([
+        ['A', 'B'],
+        ['A', 'C'],
+    ]);
+});
